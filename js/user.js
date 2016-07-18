8d14db53d2e30d827e58726dd9697acff9354e5e
Stella Ma <sm873x@gmail.com>
2016-07-17 22:55:56 -0400

    Added alert when no username.Added doc blocks to user login and fail handle

--- js/user.js
+++ js/user.js
@@ -16,14 +16,22 @@
             .fail( ns.error );
     });
 
+    /**
+     * Login to start game with username and save token and user-id data.
+     * @param  {string} username Name of user playing game
+     * @return {promise} jQuery XHR object (with promise method on it)
+     */
     ns.login = function login(username) {
         if (!username) {
-            // var def = $.Deferred();
-            // def.reject('You don\'t have a name? Really?');
-            // return def.promise();
+            alert('Please enter your name to start adventure');
+
+            var def = $.Deferred();
+            def.reject('You don\'t have a name? Really?');
+            return def.promise();
+
             console.log('no name');
-            return ns.$loginArea.text('You don\'t have a name?');
-        } else {
+        }
+
         return $.ajax({
             url: 'https://tiydc-coa-1.herokuapp.com/users/login',
             method: 'post',
@@ -36,9 +44,13 @@
                 ns.userID = data.id;
                 console.log('Token and ID saved', ns.token, ns.userID);
             });
-        }
     };
 
+    /**
+     * Displays error messages depending on status code of callback functions
+     * @param  {json} jQuery XHR object Data of callback function
+     * @return {void}   
+     */
     ns.error = function handleFail(xhr) {
         if ( 400 >= xhr.status < 500 ) {
             $(this).text('Hmmm...what did you do?');
